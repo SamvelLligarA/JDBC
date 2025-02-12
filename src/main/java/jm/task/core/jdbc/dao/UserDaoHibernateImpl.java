132eb8eb421126e668a4ce5a56915031babacfb3
Alexsandrov Mikhail <AlexandrovMD@intech.rshb.ru>
2025-02-12 20:31:30 +0400

    реализация Hibernate

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -5,8 +5,7 @@ import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.internal.StandardServiceRegistryImpl;
+
 
 import java.util.List;
 
